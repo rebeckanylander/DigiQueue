4b1b708f67fc16a3667b813abe0733387cc5a33d
rebeckanylander <rebecka.nylander@hotmail.com>
2017-12-04 16:15:43 +0100

    Lade till VM för skapa klassrum och gå med i klassrum

---
+++ DigiQueue/Models/Viewmodels/HomeIndexDigimasterVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigiQueue.Models.Viewmodels
+{
+    public class HomeIndexDigimasterVM
+    {
+        [Required(ErrorMessage = "Enter digital classroom name")]
+        public string Name { get; set; }
+        public bool ChatBox { get; set; }
+    }
+}
---
+++ DigiQueue/Models/Viewmodels/HomeIndexDigistudentVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigiQueue.Models.Viewmodels
+{
+    public class HomeIndexDigistudentVM
+    {
+        [Required(ErrorMessage="Enter alias")]
+        public string Alias { get; set; }
+        //[Required(ErrorMessage = "Choose a classroom")]
+        //public Classroom Classroom { get; set; }
+    }
+}
