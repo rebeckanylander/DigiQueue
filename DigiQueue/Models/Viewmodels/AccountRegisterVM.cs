c733395aa04c45039587e705a0112429856763bc
rebeckanylander <rebecka.nylander@hotmail.com>
2017-12-04 16:19:26 +0100

    Lade till VM för inloggning´och registrering av användare

---
+++ DigiQueue/Models/Viewmodels/AccountRegisterVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigiQueue.Models.Viewmodels
+{
+    public class AccountRegisterVM
+    {
+            public string Username { get; set; }
+            public string Password { get; set; }
+    }
+}
