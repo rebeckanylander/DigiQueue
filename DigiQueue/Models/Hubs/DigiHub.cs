2d825b54b15f1497d36dbbeeeed39360211c5c4b
tanhoa <tan.hoang@me.com>
2017-12-07 09:52:56 +0100

    Hub

--- DigiQueue/Models/Hubs/DigiHub.cs
+++ DigiQueue/Models/Hubs/DigiHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,42 @@ namespace DigiQueue.Models.Hubs
 {
     public class DigiHub : Hub
     {
-        public Task Send(string message)
+        List<Problem> waitingList = new List<Problem>();
+
+        //Skicka meddelande i infobox - digimaster
+        public Task InfoSend(string message) 
+        {
+            return Clients.All.InvokeAsync("onInfoSend", message);
+        }
+
+        //Ta bort student från waiting list - digimaster
+        public Task DeleteWaitingListItem(string alias) 
+        {
+            Problem problem = waitingList.SingleOrDefault(p => p.Alias == alias);
+            if (problem != null)
+            {
+                waitingList.Remove(problem);
+            }
+            string json = JsonConvert.SerializeObject(waitingList);
+            return Clients.All.InvokeAsync("onDeleteWaitingListItem", json);
+        }
+
+        //Skicka meddelande i chatten - digistudent
+        public Task ChatSend(string message) 
         {
             return Clients.All.InvokeAsync("onSend", message);
+        }
+
+        //Lägga till sig på listan - digistudent
+        public Task AddWaitingListItem() 
+        {
+            throw new NotImplementedException();
+        }
+
+        //Ta bort sig själv på listan - digistudent
+        public Task RemoveSelfFromWaitingList() 
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ DigiQueue/Models/Problem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigiQueue.Models
+{
+    public class Problem
+    {
+        public string Alias { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+    }
+}
